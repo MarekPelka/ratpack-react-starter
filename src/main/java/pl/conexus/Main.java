4e1377206c86f6e8858777816e5d0cd051a865c2
Marek Pelka <Marek.Pelka@coconet.pl>
2020-11-14 11:58:05 +0100

    Connected react with ratpack

--- src/main/java/pl/conexus/Main.java
+++ src/main/java/pl/conexus/Main.java
@@ -1,6 +1,12 @@
 package pl.conexus;
 
+import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
 
 import static ratpack.jackson.Jackson.json;
 
@@ -19,10 +25,27 @@ public class Main {
 	}
 
 	public static void main(String... args) throws Exception {
+
+    	// https://ratpack.io/manual/current/gradle.html#running_the_application
+		boolean isDevelopmentMode = Boolean.parseBoolean(System.getProperty("ratpack.development"));
+
+		Path staticPath;
+		if (isDevelopmentMode) {
+			System.out.println("Starting server in development mode.");
+			Map<String, String> sortedMap = new TreeMap(System.getProperties());
+			sortedMap.forEach((k, v) -> System.out.println("\t" + k + " => " + v));
+
+			String projectDirectory = System.getProperty("user.dir");
+			staticPath = Paths.get(projectDirectory, "frontend", "build");
+		} else {
+			staticPath = BaseDir.find().getRoot().resolve("static");
+		}
+
 		RatpackServer.start(server -> server
+				.serverConfig(c -> c.baseDir(staticPath))
 				.handlers(chain -> chain
-						.get(ctx -> ctx.render(json(new Person("John"))))
 						.get(":name", ctx -> ctx.render(json(new Person(ctx.getPathTokens().get("name")))))
+						.files(f -> f.indexFiles("index.html"))
 				)
 		);
 	}
