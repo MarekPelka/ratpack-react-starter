e2bbf4d118486dec19be48b247da0184ad44a2ae
Marek Pelka <Marek.Pelka@coconet.pl>
2020-11-20 22:48:46 +0100

    Product list, product modal and simple product page

--- src/main/frontend/src/components/productList.tsx
+++ src/main/frontend/src/components/productList.tsx
@@ -1,9 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from './productCard';
+import ProductCardHorizontal from './productCardHorizontal';
+
+import { Product } from '../model';
+import Modal from './productModal'
 
 export default function ProductList() {
+	const categories = [ 'All', 'Shirts', 'Sport wears', 'Outdoor' ];
+
+	const products: Array<Product> = [
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus nibh quis viverra luctus. Aliquam volutpat eget dui at commodo. Nunc arcu turpis, ultrices vitae facilisis eget, aliquet gravida turpis. Praesent blandit diam sit amet nunc sagittis, in auctor justo accumsan. Nunc at nisi enim. Pellentesque mollis iaculis turpis, vel semper nibh vestibulum in. Praesent imperdiet purus sed turpis finibus porttitor sit amet et est. Nullam vulputate congue gravida. Proin mattis erat nec lacus auctor, eu sodales sapien pretium. Fusce fermentum, turpis id sollicitudin scelerisque, lectus elit porttitor tortor, sed rhoncus dui risus sit amet sapien.',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus nibh quis viverra luctus. Aliquam volutpat eget dui at commodo. Nunc arcu turpis, ultrices vitae facilisis eget, aliquet gravida turpis. Praesent blandit diam sit amet nunc sagittis, in auctor justo accumsan. Nunc at nisi enim. Pellentesque mollis iaculis turpis, vel semper nibh vestibulum in. Praesent imperdiet purus sed turpis finibus porttitor sit amet et est. Nullam vulputate congue gravida. Proin mattis erat nec lacus auctor, eu sodales sapien pretium. Fusce fermentum, turpis id sollicitudin scelerisque, lectus elit porttitor tortor, sed rhoncus dui risus sit amet sapien.',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempus nibh quis viverra luctus. Aliquam volutpat eget dui at commodo. Nunc arcu turpis, ultrices vitae facilisis eget, aliquet gravida turpis. Praesent blandit diam sit amet nunc sagittis, in auctor justo accumsan. Nunc at nisi enim. Pellentesque mollis iaculis turpis, vel semper nibh vestibulum in. Praesent imperdiet purus sed turpis finibus porttitor sit amet et est. Nullam vulputate congue gravida. Proin mattis erat nec lacus auctor, eu sodales sapien pretium. Fusce fermentum, turpis id sollicitudin scelerisque, lectus elit porttitor tortor, sed rhoncus dui risus sit amet sapien.',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+		{
+			id: '1',
+			name: 'Gray sweater',
+			image: 'https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14.jpg',
+			shortDescription: 'Lorem ipsum sit dolor amet',
+			description: '',
+			price: 4500,
+		},
+	];
+
+	const [productModal, setProductModal] = useState(undefined as Product | undefined)
+
 	return (
 		<React.Fragment>
+			<Modal product={productModal} hideFunction={setProductModal}/>
 			<nav className="navbar navbar-expand-lg navbar-dark bg-dark mt-3 mb-5">
 				<div className="container">
 					<span className="navbar-brand">Categories:</span>
@@ -21,27 +121,15 @@ export default function ProductList() {
 
 					<div className="collapse navbar-collapse" id="basicExampleNav">
 						<ul className="navbar-nav mr-auto">
-							<li className="nav-item active">
-								<a className="nav-link" href="#">
-									All
-									<span className="sr-only">(current)</span>
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Shirts
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Sport wears
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Outwears
-								</a>
-							</li>
+							{categories.map((c) => {
+								return (
+									<li className="nav-item">
+										<a className="nav-link" href="#">
+											{c}
+										</a>
+									</li>
+								);
+							})}
 						</ul>
 
 						<form className="form-inline my-2 my-lg-0">
@@ -61,36 +149,19 @@ export default function ProductList() {
 			<div className="container">
 				<section className="text-center mb-4">
 					<div className="row">
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
-						</div>
-
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
-						</div>
-
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
-						</div>
-
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
-						</div>
-
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
-						</div>
-
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
-						</div>
-
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
-						</div>
-
-						<div className="col-lg-3 col-md-6 mb-4">
-							<ProductCard />
+						{products.map((p) => {
+							return (
+								<div className="col-lg-3 col-md-6 mb-4">
+									<div onClick={() => setProductModal(p)}>
+										<ProductCard product={p} />
+									</div>
+								</div>
+							);
+						})}
+					</div>
+					<div className="row">
+						<div className="col">
+							<ProductCardHorizontal product={products[0]}/>
 						</div>
 					</div>
 				</section>
