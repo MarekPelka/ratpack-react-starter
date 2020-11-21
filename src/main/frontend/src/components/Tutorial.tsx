1d99ce32d6dab85d3206a35399402965eea2d7ec
pbak95 <patryk.bak@gazeta.pl>
2020-11-21 16:37:42 +0100

    Change URL and add some description

--- src/main/frontend/src/components/Tutorial.tsx
+++ src/main/frontend/src/components/Tutorial.tsx
@@ -8,9 +8,23 @@ export default function Tutorial() {
                 <div className="container d-flex flex-column align-items-center">
 
                     <h2 className="my-5 h2 text-center">Jak sprzedawać w internecie?</h2>
-                    <ReactPlayer
-                        url="https://www.youtube.com/watch?v=ug50zmP9I7s"
-                    />
+                    <div className="mb-5">
+                        <ReactPlayer
+                            url="https://www.youtube.com/watch?v=1F8HZjJYnUA&t=161s"
+                        />
+                    </div>
+                    <dl className="row">
+                        <dt className="col-sm-3">Dodawanie produktow</dt>
+                        <dd className="col-sm-9">Na podstawie samouczka dowiesz się na czym skupić sie podczas doddawania swoich produktów.</dd>
+
+                        <dt className="col-sm-3">Opis produktów</dt>
+                        <dd className="col-sm-9">
+                            <p>Jak przykuć uwagę klienta, czy mniej znaczy lepiej?</p>
+                        </dd>
+
+                        <dt className="col-sm-3">Marketing internetowy</dt>
+                        <dd className="col-sm-9">Jak pozyskać i zachęcić klientów w dobie Internetu? Jakich kanałów użyć?</dd>
+                    </dl>
                 </div>
             </nav>
         </React.Fragment>
